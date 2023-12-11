406fc03bf04290cb68361e206ff158340f2c0075
oaldsp <117591173+oaldsp@users.noreply.github.com>
2023-12-11 11:03:15 -0700

    Add files via upload

--- comparador.vhd
+++ comparador.vhd
@@ -3,10 +3,12 @@ use ieee.std_logic_1164.all;
 
 
 ENTITY comparador IS
-   PORT(
+   PORT( 
 		mem   : IN  STD_LOGIC_VECTOR(7 downto 0);
 		entrada   : IN  STD_LOGIC_VECTOR(7 downto 0);
-      errou      : OUT   STD_LOGIC;
+      clk : IN	STD_LOGIC;
+		errou      : OUT   STD_LOGIC;
+		tmp      : OUT   STD_LOGIC;
 		acertou      : OUT   STD_LOGIC
 	);
 END comparador;
@@ -17,12 +19,14 @@ ARCHITECTURE funcionamento OF comparador IS
 BEGIN
 	PROCESS(mem, entrada) IS
 	BEGIN
-		IF entrada = mem THEN
-			acertou <= '1';
-			errou <= '0';
-		ELSE
-			acertou <= '0';
-			errou <= '1';
+		IF (clk'EVENT AND clk = '0') THEN
+			IF entrada = mem THEN
+				acertou <= clk;
+				errou <= '0';
+			ELSE
+				acertou <= '0';
+				errou <= '1';
+			END IF;
 		END IF;
 	END PROCESS;
 
---
+++ contadorF.vhd
@@ -0,0 +1,51 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
+ 
+entity contadorF is
+	port(
+	RSTN , clk : in std_logic;
+	Q : out std_logic_vector (3 downto 0)
+	);
+end contadorF;
+
+architecture contadorF_arch of contadorF is
+--COMPONENT--
+	component jkff is
+		port (
+		clk , J , K , PRN ,CLRN : in std_logic;
+		Q : out std_logic
+		);
+	end component;
+--COMPONENT--END--
+
+--SIGNAL--
+	signal Q_s , J_s , K_s , PRN_s , CLRN_s: std_logic_vector (3 downto 0);
+	signal clk_s  , rstn_s : std_logic;
+--SIGNAL--END--
+
+--BEGIN--
+	begin
+	gen00: for i in 0 to 3 generate
+	ffi0: jkff port map (
+		clk=>clk_s , J=>J_s(i) , K=>K_s(i) , PRN=>PRN_s(i) , CLRN=>CLRN_s(i) ,
+		Q=>Q_s(i)
+		);
+		PRN_s(i) <= '1';  
+		CLRN_s(i) <= rstn_s;
+		
+	end generate;
+	
+	K_s(0)<= '1';
+	J_s(0)<= '1';
+	K_s(1)<=Q_s(0);
+	J_s(1)<=Q_s(0);
+	K_s(2)<=(Q_s(1) and Q_s(0));
+	J_S(2)<=(Q_s(1) and Q_s(0));
+	K_s(3)<=(Q_s(2) and Q_s(1) and Q_s(0));
+	J_s(3)<=(Q_s(2) and Q_s(1) and Q_s(0));
+	
+	clk_s	<= clk;
+	rstn_s<=RSTN;
+	Q<=Q_s;
+end contadorF_arch;
